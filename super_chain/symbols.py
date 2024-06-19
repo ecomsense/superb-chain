06c90ea2bb7b6ab9608d5c32f58f3b93511c6dec
pannet1 <prog@ecomsense.in>
2024-06-19 23:19:26 +0530

    add symbol wserver

---
+++ super_chain/symbols.py
@@ -0,0 +1,91 @@
+import pandas as pd
+from constants import S_DATA, O_FUTL
+
+dct_sym = {
+    "NIFTY": {
+        "diff": 50,
+        "index": "Nifty 50",
+        "exch": "NSE",
+        "token": "26000",
+        "depth": 16,
+    },
+    "BANKNIFTY": {
+        "diff": 100,
+        "index": "Nifty Bank",
+        "exch": "NSE",
+        "token": "26009",
+        "depth": 25,
+    },
+    "MIDCPNIFTY": {
+        "diff": 100,
+        "index": "NIFTY MID SELECT",
+        "exch": "NSE",
+        "token": "26074",
+        "depth": 21,
+    },
+    "FINNIFTY": {
+        "diff": 50,
+        "index": "Nifty Fin Services",
+        "exch": "NSE",
+        "token": "26037",
+        "depth": 16,
+    },
+}
+
+
+class Symbols:
+    def __init__(self, exch: str, symbol: str, expiry: str):
+        self.exch = exch
+        self.symbol = symbol
+        self.expiry = expiry
+        self.csvfile = f"{S_DATA}/{symbol}/map_{symbol.lower()}.csv"
+
+    def get_exchange_token_map_finvasia(self):
+        if O_FUTL.is_file_not_2day(self.csvfile):
+            url = f"https://api.shoonya.com/{self.exch}_symbols.txt.zip"
+            df = pd.read_csv(url)
+            # filter the response
+            df = df[
+                (df["Exchange"] == self.exch) & (df["Symbol"] == self.symbol)
+                # & (df["TradingSymbol"].str.contains(self.symbol + self.expiry))
+            ][["Token", "TradingSymbol"]]
+            # split columns with necessary values
+            df[["Symbol", "Expiry", "OptionType", "StrikePrice"]] = df[
+                "TradingSymbol"
+            ].str.extract(r"([A-Z]+)(\d+[A-Z]+\d+)([CP])(\d+)")
+            df.to_csv(self.csvfile, index=False)
+
+    def get_atm(self, ltp) -> int:
+        current_strike = ltp - (ltp % dct_sym[self.symbol]["diff"])
+        next_higher_strike = current_strike + dct_sym[self.symbol]["diff"]
+        if ltp - current_strike < next_higher_strike - ltp:
+            return int(current_strike)
+        return int(next_higher_strike)
+
+    def find_option(self, atm, c_or_p, distance):
+        away = distance * dct_sym[self.symbol]["diff"]
+        if c_or_p == "P":
+            away = -1 * away
+        return f"{self.symbol}{self.expiry}{c_or_p}{atm + away}"
+
+    def get_all_tokens_from_csv(self):
+        df = pd.read_csv(self.csvfile)
+        dct = dict(zip(df["TradingSymbol"], df["Token"]))
+        return dct
+
+
+if __name__ == "__main__":
+    from constants import logging, O_SETG
+
+    SYMBOL = "BANKNIFTY"
+    try:
+        symbols = Symbols("NFO", SYMBOL, O_SETG[SYMBOL]["expiry"])
+        symbols.get_exchange_token_map_finvasia()
+        dct = symbols.get_all_tokens_from_csv()
+        print(dct["BANKNIFTY08MAY24C48000"])
+
+        atm = symbols.get_atm(48000.50)
+        resp = symbols.find_option(atm, "P", -2)
+        print(f"{resp=}")
+    except Exception as e:
+        logging.debug(f"{e} while getting symbols")
---
+++ super_chain/wserver.py
@@ -0,0 +1,161 @@
+from constants import logging
+
+
+class Wserver:
+    # flag to tell us if the websocket is open
+    socket_opened = False
+    ltp = {}
+
+    def __init__(self, broker, tokens, dct_tokens):
+        self.api = broker
+        self.tokens = tokens
+        self.dct_tokens = dct_tokens
+        ret = self.api.start_websocket(
+            order_update_callback=self.event_handler_order_update,
+            subscribe_callback=self.event_handler_quote_update,
+            socket_open_callback=self.open_callback,
+        )
+        if ret:
+            logging.debug(f"{ret} ws started")
+
+    def open_callback(self):
+        self.socket_opened = True
+        print("app is connected")
+        self.api.subscribe(self.tokens, feed_type="d")
+        # api.subscribe(['NSE|22', 'BSE|522032'])
+
+    # application callbacks
+    def event_handler_order_update(self, message):
+        logging.info("order event: " + str(message))
+
+    def event_handler_quote_update(self, message):
+        # e   Exchange
+        # tk  Token
+        # lp  LTP
+        # pc  Percentage change
+        # v   volume
+        # o   Open price
+        # h   High price
+        # l   Low price
+        # c   Close price
+        # ap  Average trade price
+        #
+        logging.debug(
+            "quote event: {0}".format(time.strftime("%d-%m-%Y %H:%M:%S")) + str(message)
+        )
+        val = message.get("lp", False)
+        if val:
+            exch_tkn = message.get("e", "") + "|" + message.get("tk", "")
+            token = self.dct_tokens.get(exch_tkn, None)
+
+            if token is not None:
+                # Initialize the list if it doesn't exist
+                if token not in self.ltp:
+                    self.ltp[token] = [None, None, None, None]
+
+                # Update the list only if the corresponding keys exist in the message
+                self.ltp[token][0] = float(message.get("o", self.ltp[token][0]))
+                self.ltp[token][1] = float(message.get("h", self.ltp[token][1]))
+                self.ltp[token][2] = float(message.get("l", self.ltp[token][2]))
+                self.ltp[token][3] = float(message.get("lp", self.ltp[token][3]))
+
+
+if __name__ == "__main__":
+    import time
+    import pandas as pd
+
+    stock_details_df = pd.read_csv("../../data/stocks_with_pivots_2024_01_03.csv")
+    pd_tokens = pd.read_csv("../../data/symbol_token_mapping.csv")
+    dct = (
+        pd.merge(stock_details_df["symbol"], pd_tokens, how="left", on="symbol")
+        .set_index("symbol")
+        .to_dict()
+    )
+    dct = {"NSE|" + str(v): k for k, v in dct["token"].items()}
+    lst = list(dct.keys())
+    """
+    controller = Controller()
+    controller.generate_login_object()
+    broker = controller.brokerLogin
+    wserver = Wserver(
+        broker,
+        lst,
+        dct,
+    )
+    dct_quotes = False
+    while not dct_quotes:
+        dct_quotes = wserver.ltp
+        time.sleep(1)
+    dct_quotes = {k: v
+                  for k, v in dct_quotes.items()
+                  if (v[0] == v[1]) or (v[0] == v[2])}
+    """
+    dct_quotes = {
+        "AUROPHARMA": ["1099.90", "1099.90", "1062.80", "1066.40"],
+        "HINDPETRO": ["375.60", "375.60", "364.00", "369.30"],
+        "INTELLECT": ["780.90", "814.70", "780.90", "811.70"],
+        "LAURUSLABS": ["397.15", "422.00", "397.15", "414.55"],
+        "MANAPPURAM": ["170.65", "177.55", "170.65", "176.65"],
+        "MCDOWELL-N": ["1061.00", "1084.50", "1061.00", "1076.00"],
+        "MGL": ["1203.00", "1203.00", "1184.40", "1194.70"],
+        "NATIONALUM": ["112.45", "117.80", "112.45", "115.10"],
+        "PNB": ["90.45", "90.45", "88.30", "89.25"],
+        "POLYCAB": ["5633.00", "5633.00", "5305.65", "5359.30"],
+        "PVRINOX": ["1764.00", "1764.00", "1694.55", "1700.80"],
+        "HINDZINC": ["305.05", "312.00", "305.05", "309.90"],
+        "PRESTIGE": ["1074.80", "1122.20", "1074.80", "1114.90"],
+        "VBL": ["1291.30", "1291.30", "1225.55", "1238.60"],
+    }
+
+    # convert dct_quotes values which is a list into float
+    dct_quotes = {k: [float(x) for x in v] for k, v in dct_quotes.items()}
+    print(dct_quotes)
+    lst = list(dct_quotes.keys())
+    stock_details_df = stock_details_df[stock_details_df["symbol"].isin(lst)]
+
+    lst_signals = []
+    data = {}
+    for k, v in dct_quotes.items():
+        dir_result = {}
+        prev_day_details = stock_details_df[stock_details_df["symbol"] == k].iloc[-1]
+        open_price, high, low, ltp = map(float, v)
+        if (
+            open_price == low
+            and ltp > prev_day_details["prev_high"]
+            and ltp >= prev_day_details["RES1"]
+            and ltp < prev_day_details["RES3"]
+            and (
+                (high - prev_day_details["prev_close"])
+                / prev_day_details["prev_close"]
+                * 100
+            )
+            <= 3
+            and high < (prev_day_details["RES3"] - (prev_day_details["RES3"] * 0.006))
+        ):
+            dir_result["Stock"] = k
+            dir_result["Signal"] = "B"
+            dir_result["High"] = high
+            dir_result["Low"] = low
+            dir_result["Close"] = ltp
+            lst_signals.append(dir_result)
+        elif (
+            open_price == high
+            and ltp < prev_day_details["prev_low"].astype(float)
+            and ltp < prev_day_details["SUP1"]
+            and ltp > prev_day_details["SUP3"]
+            and (
+                (low - prev_day_details["prev_close"])
+                / prev_day_details["prev_close"]
+                * 100
+            )
+            >= -3
+            and low > (prev_day_details["SUP3"] + (prev_day_details["SUP3"] * 0.006))
+        ):
+            dir_result["Stock"] = k
+            dir_result["Signal"] = "S"
+            dir_result["High"] = high
+            dir_result["Low"] = low
+            dir_result["Close"] = ltp
+            lst_signals.append(dir_result)
+        data[k] = [open_price, high, low, ltp]
+    print(lst_signals)
