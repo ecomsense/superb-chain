e016c346110711d4a3ae971189447c953d5c80bd
krishrockzz <2012501019b8@gmail.com>
2024-08-15 19:10:42 +0530

    Update gsheet_helper.py for handling api limit

--- super_chain/gsheet_helper.py
+++ super_chain/gsheet_helper.py
@@ -7,8 +7,11 @@ def authenticate_gsheet(creds_file, scope):
     client = gspread.authorize(creds)
     return client
 
-def update_gsheet(client, sheet_name,sheet_index, df, start_cell):
-    
-    sheet = client.open(sheet_name)
-    sheet_instance = sheet.get_worksheet(sheet_index)
-    sheet_instance.update([df.columns.values.tolist()] + df.values.tolist(), start_cell)
+def update_gsheet(client, sheet_name,subsheet_name, df, start_cell):
+    try:
+        sheet = client.open(sheet_name)
+        sheet_instance = sheet.worksheet(subsheet_name)
+        sheet_instance.update([df.columns.values.tolist()] + df.values.tolist(), start_cell)
+    except:
+        time.sleep(15)
+        update_gsheet(client, sheet_name,subsheet_name, df, start_cell)
